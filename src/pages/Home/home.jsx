0ff61e7f7fc710d16ac6549d475eb6f225b4f216
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-24 00:28:09 -0700

    changes: class name and img to show by theme

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,10 +3,11 @@ import './home.css';
 
 function Home() {
     const lang = useSelector(state => state.language);
+    const theme = useSelector(state => state.theme);
 
     return (
         <main>
-            <main>
+            <main className='main_body'>
                 <div className='hero_container'>
                     <div className='hero_text'>
                         <p>{lang ? 'Hola!' : 'Hello!'}</p>
@@ -20,8 +21,12 @@ function Home() {
                         </span>
                     </button>
                 </div>
-                <div className='hero_img' style={ {backgroundImage: '/assets/avatar_bg_pattern.png'}}>
-                    <img src='/images/kindpng_1600378.png' width={900} height={600} alt='avatar_img' />
+                <div className='hero_img'
+                    style={{
+                        backgroundImage: theme ? 'url(/assets/avatar_bg_pattern_light.png)' : 'url(/assets/avatar_bg_pattern.png)',
+                    }
+                }>
+                    <img src='/images/kindpng_1600378.png' alt='avatar_img' />
                 </div>
             </main>
         </main>
