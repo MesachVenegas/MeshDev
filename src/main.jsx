2337a68ca8a83f9c0a129bb7a2699c873788305e
MesachVenegas <mesach.venegas@hotmail.com>
2023-07-07 18:10:54 -0700

    add: vercel Analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { inject } from '@vercel/analytics';
 import store from './store';
 import App from './App.jsx';
 import React from 'react'
 import './index.css';
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
