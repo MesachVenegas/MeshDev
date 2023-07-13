66c5cfaeeac0fcdda05229eca1f1c2e2e3cd69b5
MesachVenegas <mesach.venegas@hotmail.com>
2023-07-13 07:59:10 -0700

    create: index to components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import ProjectCard from './ProjectCard/ProjectCard';
+import CustomButton from './Button/CustomButton';
+import Navbar from './Navbar/Navbar';
+
+export {
+    ProjectCard,
+    CustomButton,
+    Navbar
+}
