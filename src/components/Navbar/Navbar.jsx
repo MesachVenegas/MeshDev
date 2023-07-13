80b26b8fde57c8cc125769a36b15f374fa488309
MesachVenegas <mesach.venegas@hotmail.com>
2023-07-13 08:54:26 -0700

    add: motion animates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { AnimatePresence, motion } from 'framer-motion';
+import { headContainerAnimation, slideAnimation } from '../../utils/motion';
+import { useEffect } from 'react';
 import { setLanguage } from '../../store/features/language';
 import { setTheme } from '../../store/features/theme';
-import { Link } from 'react-router-dom'
 import './styles.css'
 
 const Navbar = () => {
@@ -24,60 +25,62 @@ const Navbar = () => {
     },[language])
 
     return (
-        <>
-            <nav className="nav_container">
-                <Link to='/' className="brand_container">
-                    <img className="brand_logo" src='/assets/logo_rose.svg' height={48} width={48} alt="brand_logo" />
-                    <img className="brand_text" src='/assets/text_logo.svg' height={60} width={120} alt="brand_logo" />
-                </Link>
-
-                <ul className="nav_menu">
-                    <li className="menu_item">
-                        <Link to='/' className="menu_link">
+        <AnimatePresence>
+            <motion.nav className='nav_container' {...slideAnimation('left')}>
+                <motion.div className='brand_container' {...headContainerAnimation}>
+                    <motion.a href="/" className='menu_link'>
+                        <motion.img className="brand_logo" src='/assets/logo_rose.svg' height={48} width={48} alt="brand_logo" />
+                        <motion.img className="brand_text" src='/assets/text_logo.svg' height={60} width={120} alt="brand_logo" />
+                    </motion.a>
+                </motion.div>
+                {/* navigation menu */}
+                <motion.ul className="nav_menu">
+                    <motion.li className="menu_item">
+                        <motion.a href='#home' className="menu_link">
                             <img src='/assets/home.png' height={40} width={40} alt="home-icon" />
-                            <span>{ language ? 'Inicio' :'Home'}</span>
-                        </Link>
-                    </li>
-                    <li className="menu_item">
-                        <Link to='/portfolio' className="menu_link">
+                            <span>{language ? 'Inicio' : 'Home'}</span>
+                        </motion.a>
+                    </motion.li>
+                    <motion.li className="menu_item">
+                        <motion.a href='#portfolio' className="menu_link">
                             <img src='/assets/maleting.png' height={40} width={40} alt="portfolio-icon" />
                             <span>{language ? 'Proyectos' : 'Portfolio'}</span>
-                        </Link>
-                    </li>
-                    <li className="menu_item">
-                        <Link to='/about' className="menu_link">
+                        </motion.a>
+                    </motion.li>
+                    <motion.li className="menu_item">
+                        <motion.a href='#about' className="menu_link">
                             <img src='/assets/me_avatar.png' height={40} width={40} alt="about-icon" />
                             <span>{language ? 'Sobre Mí' : 'About Me'}</span>
-                        </Link>
-                    </li>
-                    <li className="menu_item">
-                        <Link to='/skills' className="menu_link">
+                        </motion.a>
+                    </motion.li>
+                    <motion.li className="menu_item">
+                        <motion.a href='#skills' className="menu_link">
                             <img src='/assets/skills.png' height={40} width={40} alt="skills-icon" />
                             <span>{language ? 'Habilidades' : 'Skills'}</span>
-                        </Link>
-                    </li>
-                    <li className="menu_item">
-                        <Link to='/contact' className="menu_link">
+                        </motion.a>
+                    </motion.li>
+                    <motion.li className="menu_item">
+                        <motion.a href='#contact' className="menu_link">
                             <img src='/assets/contact_2.png' height={40} width={40} alt="contact-icon" />
                             <span>{language ? 'Contácteme' : 'Contact Me'}</span>
-                        </Link>
-                    </li>
-                </ul>
-
-                <div className="config_menu">
-                    <div className="config_item" onClick={() => dispatch(setLanguage(!language))}>
+                        </motion.a>
+                    </motion.li>
+                </motion.ul>
+                {/* Language and theme menu */}
+                <motion.div className="config_menu">
+                    <motion.div className="config_item" onClick={() => dispatch(setLanguage(!language))}>
                         <img src='/assets/language.png' height={32} width={32} alt="language-icon" />
                         <span className="config_text-language">{language ? 'es' : 'en'}</span>
-                    </div>
-                    <div className="config_item" onClick={() => dispatch(setTheme(!theme))}>
+                    </motion.div>
+                    <motion.div className="config_item" onClick={() => dispatch(setTheme(!theme))}>
                         <img src={theme ? '/assets/sun.png' : '/assets/moon.png'} height={32} width={32} alt="theme-icon" />
                         <span className="config_text">{theme ? 'Light' : 'Dark'}</span>
-                    </div>
-                </div>
-            </nav>
-            <div className="layer">
-            </div>
-        </>
+                    </motion.div>
+                </motion.div>
+            </motion.nav>
+            {/* layer outfocus effect */}
+            <motion.div className="layer"></motion.div>
+        </AnimatePresence>
     );
 };
 
