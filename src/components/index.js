d3074601bbe83b821dfd460c77e47607bf920812
MesachVenegas <mesach.venegas@hotmail.com>
2023-07-13 09:23:39 -0700

    change: index js to jsx

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import ProjectCard from './ProjectCard/ProjectCard';
-import CustomButton from './Button/CustomButton';
-import Navbar from './Navbar/Navbar';
-
-export {
-    ProjectCard,
-    CustomButton,
-    Navbar
-}
---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import ProjectCard from './ProjectCard';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+
+export {
+    ProjectCard,
+    CustomButton,
+    Navbar
+}
