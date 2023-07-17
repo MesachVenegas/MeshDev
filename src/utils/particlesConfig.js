9b56b017660d4d1962937db5048b12a82a5a5c7e
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-07-17 13:35:34 -0700

    add: bubble presets

--- src/utils/particlesConfig.js
+++ src/utils/particlesConfig.js
@@ -67,3 +67,63 @@ export const presets = {
     },
     detectRetina: true,
 }
+
+export const fadeBubbles ={
+    fpsLimit: 60,
+        background: {
+        color: "transparent",
+    },
+    interactivity: {
+        events: {
+            onClick: { enable: true, mode: "push" },
+            onHover: {
+                enable: true,
+                    mode: "repulse",
+                        parallax: { enable: false, force: 60, smooth: 10 }
+            },
+            resize: true
+        },
+        modes: {
+            push: { quantity: 4 },
+            repulse: { distance: 200, duration: 0.4 }
+        }
+    },
+    particles: {
+        color: { value: "#ee3a6d" },
+        move: {
+            direction: "none",
+                enable: true,
+                    outModes: "out",
+                        random: false,
+                            speed: 2,
+                                straight: false
+        },
+        number: {
+            density: {
+                enable: true,
+                    area: 800
+            },
+            value: 80
+        },
+        opacity: {
+            animation: {
+                enable: true,
+                    speed: 0.05,
+                        sync: true,
+                            startValue: "max",
+                                count: 1,
+                                    destroy: "min"
+            },
+            value: {
+                min: 0,
+                    max: 0.5
+            }
+        },
+        shape: {
+            type: "circle"
+        },
+        size: {
+            value: { min: 1, max: 5 }
+        }
+    }
+}
