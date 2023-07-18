40251cb95c046d8292d18186827743a84f7cdad1
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-07-17 22:05:51 -0700

    add: instagram link

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,10 @@ function App() {
                         <div className="welcome">
                             <span>Hi! 👋🏼</span>
                             <h1>I'am <span className='main_text'>Mesach Venegas</span></h1>
-                            <h2>Full Stack Developer</h2>
-                            <CustomTypeWritter strings={["React", "NodeJs", "Redux", "Express"]}/>
+                            <h2>FullStack Developer</h2>
+                            <CustomTypeWritter
+                                strings={["React", "NodeJs", "Redux", "Express", "PostgreSQL"]}
+                            />
                         </div>
                         <ul className="social">
                             <li className='social_item'>
@@ -27,6 +29,11 @@ function App() {
                             <li className='social_item'>
                                 <a href="#" className='social_link'>
                                     <i className="fa-brands fa-linkedin"></i>
+                                </a>
+                            </li>
+                            <li className='social_item'>
+                                <a href="#" className='social_link'>
+                                    <i class="fa-brands fa-instagram"></i>
                                 </a>
                             </li>
                         </ul>
