b95ae4de267a4ac36f1fd2fb09d319bf792de1cb
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-07-17 23:20:20 -0700

    changes: add framer motion to animate site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { CustomParticles, Navbar } from './components';
 import CustomTypeWritter from './components/CustomTypeWritter';
+import { homeWriter } from './utils/constants';
+import { AnimatePresence, motion } from 'framer-motion';
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from './utils/motion';
 
 function App() {
 
@@ -9,37 +12,40 @@ function App() {
                 <Navbar />
                 <CustomParticles />
             </header>
-            <main>
-                <section id='home' className='hero_container'>
-                    <div className='hero_content'>
-                        <div className="welcome">
-                            <span>Hi! 👋🏼</span>
-                            <h1>I'am <span className='main_text'>Mesach Venegas</span></h1>
-                            <h2>FullStack Developer</h2>
-                            <CustomTypeWritter
-                                strings={["React", "NodeJs", "Redux", "Express", "PostgreSQL"]}
-                            />
-                        </div>
-                        <ul className="social">
-                            <li className='social_item'>
-                                <a href="#" className='social_link'>
-                                    <i className="fa-brands fa-github"></i>
-                                </a>
-                            </li>
-                            <li className='social_item'>
-                                <a href="#" className='social_link'>
-                                    <i className="fa-brands fa-linkedin"></i>
-                                </a>
-                            </li>
-                            <li className='social_item'>
-                                <a href="#" className='social_link'>
-                                    <i class="fa-brands fa-instagram"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-            </main>
+            <AnimatePresence>
+                <motion.main >
+                    <motion.section id='home' className='hero_container' {...slideAnimation('down')}>
+                        <motion.div className='hero_content' {...headContainerAnimation}>
+                            <motion.div className="welcome" {...headContentAnimation}>
+                                <motion.span>Hi! 👋🏼</motion.span>
+                                <motion.h1>I'am <motion.span className='main_text'>Mesach Venegas</motion.span></motion.h1>
+                                <motion.h2>FullStack Developer</motion.h2>
+                                <CustomTypeWritter
+                                    strings={homeWriter}
+                                />
+                            </motion.div>
+                            <motion.ul className="social" {...headTextAnimation}>
+                                <motion.li className='social_item'>
+                                    <motion.a href="https://github.com/MesachVenegas" className='social_link' target='_blank'>
+                                        <motion.i className="fa-brands fa-github"></motion.i>
+                                    </motion.a>
+                                </motion.li>
+                                <motion.li className='social_item'>
+                                    <motion.a href="https://www.linkedin.com/in/mesachvenegas/" className='social_link' target='_blank'>
+                                        <motion.i className="fa-brands fa-linkedin"></motion.i>
+                                    </motion.a>
+                                </motion.li>
+                                <motion.li className='social_item'>
+                                    <motion.a href="https://www.instagram.com/mesh_dev/" className='social_link' target='_blank'>
+                                        <motion.i class="fa-brands fa-instagram"></motion.i>
+                                    </motion.a>
+                                </motion.li>
+                            </motion.ul>
+                            <motion.a {...slideAnimation('up')} href='#' className='resume_btn'>Contact Me</motion.a>
+                        </motion.div>
+                    </motion.section>
+                </motion.main>
+            </AnimatePresence>
         </>
     );
 }
