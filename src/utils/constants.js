69693da7342ad83d128165345f6a9c88f9ebf398
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-16 22:10:43 -0700

    changes: add url of projects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,9 +17,9 @@ export const projects = [
         title: "Pokedex",
         description_es: "web app con información de los pokemons existentes",
         description_en: "web app with information of the pokemons",
-        techs: ['react', 'redux'],
-        repository: "#",
-        site: "#"
+        techs: ['react', 'redux', "axios"],
+        repository: "https://github.com/MesachVenegas/PokedexWeb",
+        site: "https://pokedex-mesach.netlify.app"
     },
     {
         url: "./images/BVS_preview.png",
@@ -27,7 +27,16 @@ export const projects = [
         description_es: "Website para BVS Advising consultora chilena de diversas ramas",
         description_en: "Website for BVS Advising, a Chilean consulting company in various branches",
         techs: ['react', 'redux', "firebase"],
-        repository: "#",
-        site: "#"
+        repository: "https://github.com/MarcoDes2616/nBVS",
+        site: "https://bvsadvising.com"
+    },
+    {
+        url: "./images/Ecommerce_preview.png",
+        title: "E-commerce Shop",
+        description_es: "Proyecto E-Commerce Academlo, creación de tienda en linea con login y carrito de compras",
+        description_en: "E-Commerce Academlo project, creation of an online store with login and shopping cart",
+        techs: ['react', 'redux', "axios", "bootstrap"],
+        repository: "https://github.com/MesachVenegas/Ecommer-React",
+        site: "https://ecommer-node.netlify.app"
     },
 ]
