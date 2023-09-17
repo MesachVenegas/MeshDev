3d5b9917a32af4b89cf5d4e95d48bea920eec07d
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-16 21:20:15 -0700

    add: project data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,18 +16,18 @@ export const projects = [
         url: "./images/Pokedex_preview.png",
         title: "Pokedex",
         description_es: "web app con información de los pokemons existentes",
-        description_en: "web app with information of the pokemons"
+        description_en: "web app with information of the pokemons",
+        techs: ['react', 'redux'],
+        repository: "#",
+        site: "#"
     },
     {
         url: "./images/BVS_preview.png",
-        title: "Pokedex",
+        title: "BVS Advising",
         description_es: "Website para BVS Advising consultora chilena de diversas ramas",
-        description_en: "Website for BVS Advising, a Chilean consulting company in various branches"
-    },
-    {
-        url: "./images/project_2.jpg",
-        title: "Pokedex",
-        description_es: "web app con información de los pokemons existentes",
-        description_en: "web app with information of the pokemons"
+        description_en: "Website for BVS Advising, a Chilean consulting company in various branches",
+        techs: ['react', 'redux', "firebase"],
+        repository: "#",
+        site: "#"
     },
 ]
