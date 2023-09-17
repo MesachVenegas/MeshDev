a5385a5a004d97944b2ec6c5544a61df3e481a30
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-16 21:21:12 -0700

    add: more data and links

--- src/components/Cards/ProjectCard.jsx
+++ src/components/Cards/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
 import './style.css'
 
 function ProjectCard({ data }) {
@@ -7,10 +8,28 @@ function ProjectCard({ data }) {
     return (
         <article className="project_item">
             <figure className="card">
-                <img src={data.url} alt={data.title} title={data.title}/>
+                <img src={data.url} alt={data.title} title={data.title} />
                 <figcaption>
                     <h3>{data.title}</h3>
-                    <p>{language ?  data.description_es : data.description_en}</p>
+                    <p>{language ? data.description_es : data.description_en}</p>
+                    <div className="figure_foot">
+                        <ul className='tech_stack'>
+                            { data.techs.map( tech => (
+                                <li key={data.title}>
+                                    <img src={`./assets/${tech}.svg`} alt={tech} title={tech} />
+                                </li>
+                            ))
+                            }
+                        </ul>
+                        <ul className='links'>
+                            <li>
+                                <a href={data.repository}>Repository</a>
+                            </li>
+                            <li>
+                                <a href={data.site}>Site</a>
+                            </li>
+                        </ul>
+                    </div>
                 </figcaption>
             </figure>
         </article>
