882ff37e902ac3763f513d78ae303f7aadacc003
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-10-14 12:41:38 -0700

    add: component contact to index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,11 +3,14 @@ import CustomButton from './CustomButton';
 import CustomParticles from './CustomParticles';
 import CustomTypeWritter from './CustomTypeWritter';
 import Navbar from './Navbar';
+import Contact from './contact/Contact';
 
 export {
     ProjectCard,
     CustomButton,
     CustomParticles,
     CustomTypeWritter,
-    Navbar
+    Navbar,
+    Contact
+
 }
